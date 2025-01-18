375b9e9d4d2cbbef7a70d4f27db128b9d698da2b
Mitchell Smith <48261027+mitchierichie@users.noreply.github.com>
2025-01-18 16:41:46 -0500

    remove deps

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -16,25 +16,3 @@ rm go$VERSION.$OS-$ARCH.tar.gz
 
 INSTALLED_GO_VERSION=$(go version)
 echo "Go version ${INSTALLED_GO_VERSION} is installed"
-
-# install gopls, dlv, hey
-echo "Getting development tools"
-go install golang.org/x/tools/gopls
-go install github.com/go-delve/delve/cmd/dlv
-go install github.com/rakyll/hey
-
-# vscode-go dependencies 
-echo "Getting dependencies for the vscode-go plugin "
-# via: https://github.com/microsoft/vscode-go/blob/master/.travis.yml
-go install  github.com/acroca/go-symbols
-go install -v github.com/cweill/gotests/...
-go install -v github.com/davidrjenni/reftools/cmd/fillstruct
-go install -v github.com/haya14busa/goplay/cmd/goplay
-go install -v github.com/mdempsky/gocode
-go install -v github.com/ramya-rao-a/go-outline
-go install -v github.com/rogpeppe/godef
-go install -v github.com/sqs/goreturns
-go install -v github.com/uudashr/gopkgs/cmd/gopkgs
-go install -v github.com/zmb3/gogetdoc
-go install -v golang.org/x/lint/golint
-go install -v golang.org/x/tools/cmd/gorename
